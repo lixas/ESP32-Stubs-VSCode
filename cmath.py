481d115f827c5d5cecba7245ee4c3ea70bab7082
lixas <lixas666@gmail.com>
2019-12-27 23:50:43 +0200

    cmath and gc

--- cmath.py
+++ cmath.py
@@ -3,32 +3,128 @@ Module: 'cmath' on esp32 1.11.0
 """
 # MCU: (sysname='esp32', nodename='esp32', release='1.11.0', version='v1.11-580-g973f68780 on 2019-11-17', machine='ESP32 module with ESP32')
 # Stubber: 1.3.2
-def cos():
-    pass
 
 e = 2.718282
-def exp():
-    pass
-
-def log():
-    pass
-
-def log10():
-    pass
-
-def phase():
-    pass
+"""base of the natural logarithm"""
 
 pi = 3.141593
-def polar():
+"""the ratio of a circle’s circumference to its diameter"""
+
+def cos(z):
+    """
+    Return the cosine of z
+
+    Parameters
+    ----------
+    - z
+
+    http://docs.micropython.org/en/latest/library/cmath.html#cmath.cos
+
+    """
+
     pass
 
-def rect():
+def exp(z) -> exponential:
+    """
+    Return the exponential of z
+    
+    Parameters
+    ----------
+    - z
+
+    http://docs.micropython.org/en/latest/library/cmath.html#cmath.exp
+
+    """
     pass
 
-def sin():
+def log(z) -> NaturalLogarithm:
+    """
+    Return the natural logarithm of z. The branch cut is along the negative real axis.
+    
+    Parameters
+    ----------
+    - z
+
+    http://docs.micropython.org/en/latest/library/cmath.html#cmath.log
+
+    """
     pass
 
-def sqrt():
+def log10(z) -> logarithm:
+    """
+    Return the base-10 logarithm of z. The branch cut is along the negative real axis.
+    
+    Parameters
+    ----------
+    - z
+
+    http://docs.micropython.org/en/latest/library/cmath.html#cmath.log10
+
+    """
+    pass
+
+def phase(z) -> phase:
+    """
+    Returns the phase of the number z, in the range (-pi, +pi].
+
+    Parameters
+    ----------
+    - z
+
+    http://docs.micropython.org/en/latest/library/cmath.html#cmath.phase
+
+    """
+    pass
+
+def polar(z) -> tuple:
+    """
+    Returns, as a tuple, the polar form of z.
+
+    Parameters
+    ----------
+    - z
+
+    http://docs.micropython.org/en/latest/library/cmath.html#cmath.polar
+
+    """
+    pass
+
+def rect(r, phi) -> ComplexNumber:
+    """
+    Returns the complex number with modulus r and phase phi.
+
+    Parameters
+    - r
+    - phi
+
+    http://docs.micropython.org/en/latest/library/cmath.html#cmath.rect
+
+    """
+    pass
+
+def sin(z) -> sine:
+    """
+    Return the sine of z.
+
+    Parameters
+    ----------
+    - z
+
+    http://docs.micropython.org/en/latest/library/cmath.html#cmath.sin
+
+    """
+    pass
+
+def sqrt(z):
+    """
+    Return the square-root of z.
+
+    Parameters
+    ----------
+    - z
+
+    http://docs.micropython.org/en/latest/library/cmath.html#cmath.sqrt
+    
+    """
     pass
 
--- gc.py
+++ gc.py
@@ -4,23 +4,71 @@ Module: 'gc' on esp32 1.11.0
 # MCU: (sysname='esp32', nodename='esp32', release='1.11.0', version='v1.11-580-g973f68780 on 2019-11-17', machine='ESP32 module with ESP32')
 # Stubber: 1.3.2
 def collect():
+    """
+    Run a garbage collection.
+
+    http://docs.micropython.org/en/latest/library/gc.html#gc.collect
+
+    """
     pass
 
 def disable():
+    """
+    Disable automatic garbage collection. Heap memory can still be allocated, and garbage collection can still be initiated manually using gc.collect().
+
+    http://docs.micropython.org/en/latest/library/gc.html#gc.disable
+
+    """
     pass
 
 def enable():
+    """
+    Enable automatic garbage collection.
+
+    http://docs.micropython.org/en/latest/library/gc.html#gc.enable
+
+    """
     pass
 
-def isenabled():
+def isenabled() -> boolean:
+    """
+    Returns the state of garbage collector
+
+    """
     pass
 
 def mem_alloc():
+    """
+    Return the number of bytes of heap RAM that are allocated.
+
+    http://docs.micropython.org/en/latest/library/gc.html#gc.mem_alloc
+
+    """
     pass
 
 def mem_free():
+    """
+    Return the number of bytes of available heap RAM, or -1 if this amount is not known.
+
+    http://docs.micropython.org/en/latest/library/gc.html#gc.mem_free
+
+    """
     pass
 
-def threshold():
+def threshold(amount):
+    """
+    Set or query the additional GC allocation threshold. 
+
+    Parameters
+    ----------
+    - [amount] : 
+    
+    Normally, a collection is triggered only when a new allocation cannot be satisfied, i.e. on an out-of-memory (OOM) condition. If this function is called, in addition to OOM, a collection will be triggered each time after amount bytes have been allocated (in total, since the previous time such an amount of bytes have been allocated) amount is usually specified as less than the full heap size, with the intention to trigger a collection earlier than when the heap becomes exhausted, and in the hope that an early collection will prevent excessive memory fragmentation. This is a heuristic measure, the effect of which will vary from application to application, as well as the optimal value of the amount parameter.
+
+    Calling the function without argument will return the current value of the threshold. A value of -1 means a disabled allocation threshold.
+
+    http://docs.micropython.org/en/latest/library/gc.html#gc.threshold
+    
+    """
     pass
 
